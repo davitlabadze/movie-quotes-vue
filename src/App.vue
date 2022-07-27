7756cde7db687d583b651b50f64828e56d03631d
davitlabadze <labadzedato18@gmail.com>
2022-07-27 18:52:48 +0400

    docs: add heroicon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <header>
     <h1 class="text-3xl font-bold underline">Hello World</h1>
+    <div>
+      <BeakerIcon class="w-5 h-5 text-blue-500" />
+      <p>...</p>
+    </div>
   </header>
 </template>
+
+<script>
+import { BeakerIcon } from "@heroicons/vue/solid";
+
+export default {
+  components: { BeakerIcon },
+};
+</script>
