30f38a979718f7d562c39b1cfef7541a406827d1
davitlabadze <labadzedato18@gmail.com>
2022-07-30 12:17:25 +0400

    style: create dashboard table

--- src/adminPanel/DashboardItem.vue
+++ src/adminPanel/DashboardItem.vue
@@ -5,25 +5,30 @@
         color="bg-gradient-to-r from-blue-500 to-cyan-500 shadow-lgshadow-cyan-500/50 hover:from-indigo-500 hover:to-pink-500 hover:shadow-red-600/50"
         name="All Movies"
         path="/admin/movies"
+        :data="25"
         :icon="FilmIcon"
       />
       <DashboardCard
         color="bg-gradient-to-r from-green-500 to-teal-500 shadow-lg shadow-teal-500/50 hover:from-yellow-500 hover:to-red-500 hover:shadow-green-600/50"
         name="All Quotes"
         path="/admin/quotes"
+        :data="200"
         :icon="BookOpenIcon"
       />
     </div>
+    <DashboardTable />
   </div>
 </template>
 
 <script>
 import DashboardCard from "../components/adminPanel/DashboardCard.vue";
 import { FilmIcon, BookOpenIcon } from "@heroicons/vue/outline";
+import DashboardTable from "../components/adminPanel/DashboardTable.vue";
 
 export default {
   components: {
     DashboardCard,
+    DashboardTable,
   },
   data() {
     return {
---
+++ src/components/adminPanel/DashboardTable.vue
@@ -0,0 +1,256 @@
+<template>
+  <div class="flex py-3 mt-10">
+    <ClipboardListIcon
+      class="flex-shrink-0 w-6 h-6 mr-3 text-black dark:text-slate-600"
+    />
+
+    <p class="dark:text-slate-600">Recent Movies quotes</p>
+  </div>
+  <table
+    class="w-full text-center divide-y divide-gray-200 rounded-lg shadow-md dark:divide-slate-700"
+  >
+    <thead
+      class="flex w-full text-white rounded-t-lg bg-gray-50 dark:bg-slate-800"
+    >
+      <tr class="flex w-full mb-2">
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          id
+        </th>
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          movie english
+        </th>
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          movie georiga
+        </th>
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          quote english
+        </th>
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          quote georgia
+        </th>
+        <th
+          class="w-1/4 p-2 text-xs text-gray-500 uppercase dark:text-slate-600"
+        >
+          image
+        </th>
+      </tr>
+    </thead>
+    <tbody
+      class="flex flex-col items-center w-full overflow-x-hidden overflow-y-scroll bg-white rounded-b-lg dark:bg-slate-800 h-96"
+    >
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+      <tr class="flex w-full bg-white dark:bg-slate-800">
+        <td class="w-1/4 p-4 px-6 text-gray-900 dark:text-slate-600">1</td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          movie georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote english
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          quote georgia
+        </td>
+        <td class="w-1/4 p-4 px-6 text-gray-500 dark:text-slate-600">
+          <img
+            class="ml-20"
+            src="https://lh3.googleusercontent.com/g-W0vwcMvALYROKIaS0t0CQrVKZ6cqqFiibzQ0B9-TLkmEkBmYDyBCgl9RvYDStP-2lzvRBqr6v5wxgaoVlujwEP=w640-h400-e365-rj-sc0x00ffffff"
+            width="64"
+            height="64"
+            alt="movie img"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { ClipboardListIcon } from "@heroicons/vue/outline";
+export default {
+  components: {
+    ClipboardListIcon,
+  },
+};
+</script>
+
+<style></style>
