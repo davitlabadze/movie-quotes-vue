ab9d080343086e2a101b27c29451269cd7bb2fb3
davitlabadze <labadzedato18@gmail.com>
2022-07-30 20:12:43 +0400

    style: edit movie page

--- src/adminPanel/movies/EditMovie.vue
+++ src/adminPanel/movies/EditMovie.vue
@@ -1,9 +1,62 @@
 <template>
-  <h1>Edit Movie</h1>
+  <ActionItem
+    title="Edit Movie"
+    path="movies"
+    action="All Movies"
+    :icon="TableIcon"
+    :btnIcon="EyeIcon"
+  />
+  <div>
+    <form class="mt-10">
+      <div class="mb-6">
+        <label
+          class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+          htmlFor="movie_en"
+        >
+          movie english
+        </label>
+        <input
+          class="w-full p-2 border border-gray-400 rounded outline-none dark:border-slate-700 dark:bg-slate-800 dark:text-slate-600"
+          type="text"
+          name="movie-en"
+          id="movie-en"
+        />
+      </div>
+
+      <div class="mb-6">
+        <label
+          class="block mb-2 text-xs font-bold text-gray-700 uppercase"
+          htmlFor="movie_ka"
+        >
+          Movie georiga
+        </label>
+        <input
+          class="w-full p-2 border border-gray-400 rounded outline-none dark:bg-slate-800 dark:text-slate-600 dark:border-slate-700"
+          type="text"
+          name="movie-ka"
+          id="movie-ka"
+        />
+      </div>
+      <div class="flex mb-6 w-min">
+        <ButtonItem title="Create" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import { TableIcon, EyeIcon } from "@heroicons/vue/outline";
+import ButtonItem from "../../components/adminPanel/ButtonItem.vue";
+import ActionItem from "../../components/adminPanel/ActionItem.vue";
+export default {
+  data() {
+    return {
+      TableIcon,
+      EyeIcon,
+    };
+  },
+  components: { ButtonItem, ActionItem },
+};
 </script>
 
 <style></style>
