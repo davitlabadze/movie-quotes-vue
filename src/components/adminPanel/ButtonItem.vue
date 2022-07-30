177bc008b1e8fe5980fb29829d7f1d27126ee184
davitlabadze <labadzedato18@gmail.com>
2022-07-30 20:10:43 +0400

    style: create movie page

---
+++ src/components/adminPanel/ButtonItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    type="submit"
+    className="w-full px-4 py-2 text-white bg-green-600 rounded-lg rounderd hover:bg-green-700 dark:bg-sky-400 dark:hover:bg-sky-500 dark:text-slate-700"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+
+<style></style>
