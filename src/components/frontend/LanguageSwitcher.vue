829bd8d7009ef85bab9a99e0f06adaa1729e5053
davitlabadze <labadzedato18@gmail.com>
2022-08-17 02:50:49 +0400

    style(LanguageSwitcher): change the color of the switch

--- src/components/frontend/LanguageSwitcher.vue
+++ src/components/frontend/LanguageSwitcher.vue
@@ -2,12 +2,14 @@
   <div class="fixed px-12 space-y-3 py-80">
     <div
       class="w-12 h-12 px-3 py-3 mb-2 text-sm text-center text-white border-2 rounded-full cursor-pointer"
+      :class="this.$i18n.locale === 'ka' ? 'text-black bg-white' : ''"
     >
       <button @click="setLocale('en')">en</button>
     </div>
 
     <div
-      class="w-12 h-12 px-3 py-3 mb-2 text-sm text-center bg-white border-2 rounded-full cursor-pointer"
+      class="w-12 h-12 px-3 py-3 mb-2 text-sm text-center text-white border-2 rounded-full cursor-pointer"
+      :class="this.$i18n.locale === 'en' ? 'text-black bg-white' : ''"
     >
       <button @click="setLocale('ka')">ka</button>
     </div>
