37b553fb2e01dbf8255fae44de4bcf240544d960
davitlabadze <labadzedato18@gmail.com>
2022-08-21 14:31:24 +0400

    refactor(LoginForAdmin): change route

--- src/components/frontend/LoginForAdmin.vue
+++ src/components/frontend/LoginForAdmin.vue
@@ -2,8 +2,19 @@
   <div
     class="fixed ml-10 text-sm text-gray-600 underline opacity-50 bottom-16 hover:text-gray-300"
   >
-    <router-link :to="{ name: 'home.login' }">
-      <h1 class="font-sans">{{ $t("Authorization for the administrator") }}</h1>
+    <router-link
+      :to="token ? { name: 'admin.dashboard' } : { name: 'home.login' }"
+    >
+      <h1 class="font-sans">
+        {{
+          token
+            ? $t("Go to Adminpanel")
+            : $t("Authorization for the administrator")
+        }}
+      </h1>
     </router-link>
   </div>
 </template>
+<script setup>
+const token = localStorage.getItem("token");
+</script>
