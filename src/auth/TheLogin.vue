a18aa2f6c968196c8b390113c1949ee0fb80562c
davitlabadze <labadzedato18@gmail.com>
2022-08-22 14:31:44 +0400

    refactor(login): add validation rule

--- src/auth/TheLogin.vue
+++ src/auth/TheLogin.vue
@@ -8,7 +8,7 @@
         <h1 class="text-center front-bold-text-xl">
           {{ $t("Authorization") }}
         </h1>
-        <form @submit.prevent="handleLogin">
+        <Form @submit="handleLogin">
           <div class="mb-6">
             <label
               class="block mb-2 text-xs font-bold text-gray-700 uppercase"
@@ -16,12 +16,18 @@
             >
               {{ $t("email") }}
             </label>
-            <input
+            <Field
               class="w-full p-2 border border-gray-400"
               type="email"
-              v-model="email"
+              name="email"
+              rules="required"
             />
+            <span className="mt-2 text-xs text-red-500">
+              <ErrorMessage name="email" />
+              {{ errStatus == 404 ? $t("This user does not exist") : "" }}
+            </span>
           </div>
+
           <div class="mb-6">
             <label
               class="block mb-2 text-xs font-bold text-gray-700 uppercase"
@@ -29,49 +35,63 @@
             >
               {{ $t("password") }}
             </label>
-            <input
+            <Field
               class="w-full p-2 border border-gray-400"
               type="password"
               name="password"
-              id="password"
-              v-model="password"
+              autocomplete="on"
+              rules="required"
             />
+            <span className="mt-2 text-xs text-red-500">
+              <ErrorMessage name="password" />
+              {{ errStatus == "401" ? $t("The password is incorrect") : "" }}
+            </span>
           </div>
           <div class="flex mb-6">
             <button
               type="submit"
-              class="px-4 py-2 text-white bg-gray-400 rounderd hover:bg-gray-500"
+              class="px-4 py-2 text-white bg-gray-400 hover:bg-gray-500 rounderd"
             >
               {{ $t("Log In") }}
             </button>
           </div>
-        </form>
+        </Form>
       </main>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 const router = useRouter();
 import BackButton from "../components/frontend/BackButton.vue";
-import LoginAPI from "../services/LoginAPI";
+import AuthAPI from "../services/AuthAPI";
+import { defineRule, Form, Field, ErrorMessage } from "vee-validate";
 
-// #issues  change ref form input value
-const email = "admin@admin.ge";
-const password = "admin";
+const locale = localStorage.getItem("lang");
+defineRule("required", (value) => {
+  if (!value || !value.length) {
+    if (locale === "en") return "Value is required";
+    if (locale === "ka") return "ველი ცარიელია";
+  }
+  return true;
+});
 
-const handleLogin = async () => {
+const errStatus = ref(null);
+const handleLogin = async (values) => {
   try {
-    const response = await LoginAPI.Authorization(email, password);
-    const checkUser = response?.data.user;
+    const response = await AuthAPI.login(values.email, values.password);
+    const checkUser = response?.data?.user;
     if (checkUser) {
       localStorage.setItem("token", response.data.token);
-      router.push({ name: "admin.dashboard" });
+      router.replace({ name: "admin.dashboard" });
     } else {
+      errStatus.value = response.data.status;
       console.log(response.data.status);
     }
   } catch (err) {
+    errStatus.value = 404;
     console.error(err);
   }
 };
